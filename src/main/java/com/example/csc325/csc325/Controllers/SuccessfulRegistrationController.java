154ea84a007428f6003aef94e5aebf534395093d
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-12-10 17:17:05 -0500

    Extracted displayJobs

--- src/main/java/com/example/csc325/csc325/Controllers/SuccessfulRegistrationController.java
+++ src/main/java/com/example/csc325/csc325/Controllers/SuccessfulRegistrationController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class SuccessfulRegistrationController {
     @FXML
@@ -14,8 +15,8 @@ public class SuccessfulRegistrationController {
     @FXML
     public Button backToLoginBtn;
 
-    public void backToLogin() throws IOException {
-        SceneManager.getInstance().showLoginScene();
+    public void backToLogin() throws IOException, ExecutionException, InterruptedException {
+        SceneManager.getInstance().showMainScene();
     }
 
 
