968699bc9dc649cac99d6f3f814810dd72c0b066
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-12-10 19:12:06 -0500

    Implemented regex

--- src/main/java/com/example/csc325/csc325/Controllers/employerSignupController.java
+++ src/main/java/com/example/csc325/csc325/Controllers/employerSignupController.java
@@ -17,9 +17,10 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class employerSignupController {
-
 
     public TextField companyNameField;
     public TextField emailField;
@@ -31,21 +32,46 @@ public class employerSignupController {
 
     public Label lblErrorMsg;
 
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
+    private static final String PHONE_REGEX = "^\\d{10}$"; // Assumes a 10-digit phone number
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+
     public void backToLogin(ActionEvent actionEvent) throws IOException {
         SceneManager.getInstance().showLoginScene();
     }
 
     public void handleSignUp(ActionEvent actionEvent) throws ExecutionException, InterruptedException {
         Firestore db = FirestoreClient.getFirestore();
-        if (companyNameField.getText().isEmpty() || emailField.getText().isEmpty() || passwordField.getText().isEmpty() || ConfirmPasswordField.getText().isEmpty() || phoneNumberField.getText().isEmpty()) {
+
+        // Validate email using Regex
+        if (!emailField.getText().matches(EMAIL_REGEX)) {
+            System.out.println("Invalid email format");
+            lblErrorMsg.setText("Invalid email format");
+            return;
+        }
+
+        // Validate phone number using Regex
+        if (!phoneNumberField.getText().matches(PHONE_REGEX)) {
+            System.out.println("Invalid phone number format");
+            lblErrorMsg.setText("Invalid phone number format");
+            return;
+        }
+
+        // Validate password using Regex
+        if (!passwordField.getText().matches(PASSWORD_REGEX)) {
+            System.out.println("Invalid password format");
+            lblErrorMsg.setText("Invalid password format");
+            return;
+        }
+
+        if (companyNameField.getText().isEmpty() || emailField.getText().isEmpty() ||
+                passwordField.getText().isEmpty() || ConfirmPasswordField.getText().isEmpty() ||
+                phoneNumberField.getText().isEmpty()) {
             System.out.println("All fields are required");
             lblErrorMsg.setText("All fields are required");
-
-        }
-        else if (!passwordField.getText().equals(ConfirmPasswordField.getText())) {
-            System.out.println("Passwords dont match");
-            lblErrorMsg.setText("Passwords dont match");
-
+        } else if (!passwordField.getText().equals(ConfirmPasswordField.getText())) {
+            System.out.println("Passwords don't match");
+            lblErrorMsg.setText("Passwords don't match");
         } else {
             ApiFuture<QuerySnapshot> query = db.collection("auth")
                     .whereEqualTo("username", emailField.getText())
@@ -56,8 +82,8 @@ public class employerSignupController {
                 System.out.println("Account using email already exists");
                 lblErrorMsg.setText("Account using email already exists");
                 return;
-
             }
+
             User user = new Employer(companyNameField.getText(), emailField.getText(), phoneNumberField.getText());
             user.register(passwordField.getText());
         }
